a5a495122f61cdf3e5b5c3c4f59f5f05ac8741f7
Dnartreb <bertrand.lagarde@gmail.com>
2025-09-25 10:58:11 +0200

    HomeView styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,7 +12,7 @@ const { getRouteFor } = useLocalization()
 
 <template>
   <!-- Hero Section -->
-  <section class="max-w-7xl mx-auto flex flex-col gap-3 py-8 sm:py-16 px-4 lg:px-8 justify-center text-primary">
+  <section class="max-w-7xl mx-auto flex flex-col gap-3 py-8 sm:py-16 px-4 lg:px-8 justify-center">
     <div class="flex flex-col leading-normal">
       <h1 class="font-semibold lg:text-5xl md:text-4xl sm:text-3xl text-2xl uppercase" v-html="t('home.hero.title')" />
       <p class="font-semibold lg:text-5xl md:text-4xl sm:text-3xl text-2xl uppercase" v-html="t('home.hero.subtitle1')" />
@@ -31,6 +31,9 @@ const { getRouteFor } = useLocalization()
     </div>
   </section>
 
+  <!-- Section Divider -->
+  <hr class="h-0 border-t-2 border-primary/20 border-dashed" />
+
   <!-- Hero Image -->
   <section 
     class="h-[180px] bg-[center_-40px] sm:bg-[center_-270px] relative w-full overflow-hidden bg-cover bg-fixed bg-no-repeat" 
@@ -39,26 +42,29 @@ const { getRouteFor } = useLocalization()
     :aria-label="t('home.hero.imageAlt')"
   />
 
+  <!-- Section Divider -->
+  <hr class="h-0 border-t-2 border-primary/20 border-dashed" />
+
   <!-- Solution Section -->
-  <section class="max-w-7xl mx-auto my-8 sm:my-16 px-4 lg:px-8">
+  <section class="max-w-7xl mx-auto p-4 lg:p-8">
     <h2 v-html="t('blocks.solution.title')" class="font-semibold text-3xl mb-4" />
-    <div class="grid grid-cols-1 lg:grid-cols-3 gap-8">
+    <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
       <article class="basis-full p-4 border-2 border-primary/20 hover:border-primary/40 border-dashed rounded-lg">
-        <h3 class="font-semibold text-xl mb-2 flex gap-2 items-center">
+        <h3 class="font-semibold xl:text-xl sm:text-lg mb-2 flex gap-2 items-center">
           <FileSignIcon class="w-6 h-6"></FileSignIcon>
           <span v-html="t('blocks.solution.structuration.title')"></span>
         </h3>
         <div v-html="t('blocks.solution.structuration.description')" class="text-base leading-relaxed" />
       </article>
       <article class="basis-full p-4 border-2 border-primary/20 hover:border-primary/40 border-dashed rounded-lg">
-        <h3 class="font-semibold text-xl mb-2 flex gap-2 items-center">
+        <h3 class="font-semibold xl:text-xl sm:text-lg mb-2 flex gap-2 items-center">
           <CubeScanIcon class="w-6 h-6"></CubeScanIcon>
           <span v-html="t('blocks.solution.tokenization.title')"></span>
         </h3>
         <div v-html="t('blocks.solution.tokenization.description')" class="text-base leading-relaxed" />
       </article>
       <article class="basis-full p-4 border-2 border-primary/20 hover:border-primary/40 border-dashed rounded-lg">
-        <h3 class="font-semibold text-xl mb-2 flex gap-2 items-center">
+        <h3 class="font-semibold xl:text-xl sm:text-lg mb-2 flex gap-2 items-center">
           <AccountGroupIcon class="w-6 h-6"></AccountGroupIcon>
           <span v-html="t('blocks.solution.distribution.title')"></span>
         </h3>
@@ -71,23 +77,23 @@ const { getRouteFor } = useLocalization()
   <hr class="h-0 border-t-2 border-primary/20 border-dashed" />
 
   <!-- Why Section -->
-  <section class="max-w-7xl mx-auto my-8 sm:my-16 px-4 lg:px-8">
+  <section class="max-w-7xl mx-auto p-4 lg:p-8">
     <h2 v-html="t('blocks.why.title')" class="font-semibold text-3xl mb-4" />
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
       <article class="basis-full p-4 border-2 border-primary/20 hover:border-primary/40 border-dashed rounded-lg">
-        <h3 v-html="t('blocks.why.regulation.title')" class="font-semibold text-xl mb-2" />
+        <h3 v-html="t('blocks.why.regulation.title')" class="font-semibold xl:text-xl sm:text-lg mb-2" />
         <div v-html="t('blocks.why.regulation.description')" class="text-base leading-relaxed" />
       </article>
       <article class="basis-full p-4 border-2 border-primary/20 hover:border-primary/40 border-dashed rounded-lg">
-        <h3 v-html="t('blocks.why.yield.title')" class="font-semibold text-xl mb-2" />
+        <h3 v-html="t('blocks.why.yield.title')" class="font-semibold xl:text-xl sm:text-lg mb-2" />
         <div v-html="t('blocks.why.yield.description')" class="text-base leading-relaxed" />
       </article>
       <article class="basis-full p-4 border-2 border-primary/20 hover:border-primary/40 border-dashed rounded-lg">
-        <h3 v-html="t('blocks.why.impact.title')" class="font-semibold text-xl mb-2" />
+        <h3 v-html="t('blocks.why.impact.title')" class="font-semibold xl:text-xl sm:text-lg mb-2" />
         <div v-html="t('blocks.why.impact.description')" class="text-base leading-relaxed" />
       </article>
       <article class="basis-full p-4 border-2 border-primary/20 hover:border-primary/40 border-dashed rounded-lg">
-        <h3 v-html="t('blocks.why.innovation.title')" class="font-semibold text-xl mb-2" />
+        <h3 v-html="t('blocks.why.innovation.title')" class="font-semibold xl:text-xl sm:text-lg mb-2" />
         <div v-html="t('blocks.why.innovation.description')" class="text-base leading-relaxed" />
       </article>
     </div>
