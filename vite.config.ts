e2e3069595cb181937567efb182c769c7dc37ef9
Dnartreb <bertrand.lagarde@gmail.com>
2025-10-08 14:38:19 +0200

    env var BASE_PATH  for vite build on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/web/': '',
-  plugins: [
-    tailwindcss(),
-    vue(),
-    vueDevTools()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    base: env.BASE_PATH ?? '',
+    plugins: [
+      tailwindcss(),
+      vue(),
+      vueDevTools()
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+    }
   }
-}))
+})
