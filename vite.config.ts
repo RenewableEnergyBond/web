841683b37678ddd553d19b3637aaf94c4cc386e9
Dnartreb <bertrand.lagarde@gmail.com>
2025-10-13 14:07:34 +0200

    Fix: Set default base path to '/' in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig(({ mode, command }) => {
   const env = loadEnv(mode, process.cwd());
-  const base = process.env.BASE_PATH || env.BASE_PATH || '';
+  const base = process.env.BASE_PATH || env.BASE_PATH || '/';
   return {
     base,
     plugins: [
